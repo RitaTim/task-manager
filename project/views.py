d67f937ffb97f46df860a665432985fe4556ad41
rita <margaritachka@gmail.com>
2015-02-27 05:05:03 +0300

    feature/project: module form edit

--- project/views.py
+++ project/views.py
@@ -8,67 +8,33 @@ from forms import ProjectForm
 from django.contrib.auth.models import User
 import logging
 
-def create(request):
-	if request.method == 'POST':
-		form = ProjectForm(request.POST)
-		if form.is_valid():
-			form.save()
-		return HttpResponse("Вы создали и сохранили новую форму проекта")
-	else:
-		form = ProjectForm()
-		args = {}
-		args.update(csrf(request))
-		args['form'] = form
-		return HttpResponse("Вы не создали новую форму проекта. И остались на странице создания")
-
-def new(request):
+def edit_project(request, id_project = '0'):
 	if request.method == "POST":
-		form = ProjectForm(request.POST, request.FILES)
-		if form.is_valid():
-			#leader = User.objects.get(username = request.POST['name_leader'])
-			#request.POST['leader_id'] = leader.id
-			form.save()
-			return redirect('/projects')
-		return HttpResponse("Форма не валидна")
-
-	#users = User.objects.all()
-	user = request.user
-
-	args = {}
-	#args['users'] = users
-	args.update(csrf(request))
-	args['user'] = user
-	args['form'] = ProjectForm()
-	args['projects'] = Project.objects.all()	
-	return render_to_response('new.html', args)
-
-def edit_project(request, id_project = 0):
-	if request.method == "POST":
-		url = ''
-		if id_project:
+		if id_project != '0':
 			project = Project.objects.get(id = id_project) 
 			form 	= ProjectForm(request.POST, instance = project)
-			url  	= "/projects/project/" + str(id_project)
 		else:
 			form = ProjectForm(request.POST, request.FILES)
-			url  = "/projects"
 
 		if not form.is_valid():
 			return HttpResponse("Форма не валидна")
 		
 		form.save()	
 
-		return redirect(url)
+		return redirect(request.META.get('HTTP_REFERER','/'))
 	else: # GET
 		args={}
 		args.update(csrf(request))	
 
-		if id_project:
+		if 'id_project' in request.GET:
+			id_project = request.GET['id_project']
+		if id_project != '0':
 			project = Project.objects.get(id = id_project)
 			args['form'] = ProjectForm(instance = project)
+			args['project'] = Project.objects.get(id = id_project)
 		else:
 			args['form'] = ProjectForm()
-		args['project'] = Project.objects.get(id = id_project)
+		
 
 		return render_to_response('project_edit.html', args)
 
