92ea999e83c36b85ea87841ece0ea88efadaeb4c
rita <margaritachka@gmail.com>
2015-04-10 08:18:16 +0300

    feature/fix_forum_page

--- forum/models.py
+++ forum/models.py
@@ -1,12 +1,13 @@
 #-*-coding: utf-8 -*-
 from django.db import models
 from project.models import Project
+from django.core.cache import cache
 
 # Create your models here.
 class Forum(models.Model):
 	title = models.CharField(max_length = 200)	
 
-	project = models.ForeignKey(Project, verbose_name = "Проект", default=lambda: Project.objects.get(id=1))
+	project = models.ForeignKey(Project, verbose_name = "Проект", default=lambda: Project.objects.get(id=cache.get('project_id')))
 
 
 	def __unicode__(self):
